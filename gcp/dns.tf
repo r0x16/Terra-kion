d7daca7e409c02dd4d5a6d2304cf0a46dd418578
r0x16 <sir.thomas.lara@gmail.com>
2023-06-11 21:19:19 -0400

    feat(gcp): create cloud dns register (#12)
    - Add dns.tf file to create cloud dns register
    - Update .terraform.lock.hcl file to reflect last used versions

---
+++ gcp/dns.tf
@@ -0,0 +1,34 @@
+# This file defines the domain name using Cloud DNS
+# Creates:
+# - Cloud DNS zone
+# - Cloud DNS record set
+#
+# Requires:
+# - Published Kubernetes service
+#
+
+# Define the DNS zone
+module "terra-kion-dns" {
+  source  = "terraform-google-modules/cloud-dns/google"
+  version = "5.0"
+  project_id = var.project
+  type = "public"
+  name = "terra-kion-cloud"
+  domain = "terra-kion.cloud."
+  
+  # Define the DNS record set
+  recordsets = [
+    {
+        name = ""
+        type = "A"
+        ttl = 300 # 5 minutes
+        records = [ kubernetes_service.terra-kion.status[0].load_balancer[0].ingress[0].ip ]
+    },
+    {
+        name = "www"
+        type = "CNAME"
+        ttl = 300 # 5 minutes
+        records = [ "terra-kion.cloud." ]
+    }
+  ]
+}
